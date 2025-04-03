1876eafcaed05ce3257a3f45670024f74d31b3bb
Muthubharathi Renganathan <muthubharathi.r@gmail.com>
2025-04-03 19:38:27 +0530

    Initial commit

---
+++ src/main/java/com/pa/api/personalauditor/PersonalAuditorApplication.java
@@ -0,0 +1,13 @@
+package com.pa.api.personalauditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalAuditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalAuditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pa/api/personalauditor/PersonalAuditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pa.api.personalauditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalAuditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
